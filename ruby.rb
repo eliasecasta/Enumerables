881b7455b7923312fe9b81373bc67715c8f69f0d
jessica <jessica.farias.rosado@gmail.com>
2020-06-22 17:26:41 -0500

    Adding my_each method

--- ruby.rb
+++ ruby.rb
@@ -1,5 +1,24 @@
+# frozen_string_literal: true
+
+# Some comment
+# Enumerable
 module Enumerable
-    def my_each
-      # your code here
+  def my_each
+    if is_a?(Array)
+      length.times do |i|
+        yield self[i]
+      end
+    else
+      length.times do |i|
+        yield [keys[i], values[i]]
+      end
     end
+  end
 end
+friends = %w[Jessica Michelle Elias Castañeda]
+friends.each { |friend| puts 'Hello, ' + friend }
+my_hash = { 'one' => 5, 'two' => 2 }
+my_hash.my_each { |key, value| puts "#{key} is #{value}" }
+my_hash.my_each { |pair| puts "the pair is #{pair}" }
+my_hash.each { |key, value| puts "#{key} is #{value}" }
+my_hash.each { |pair| puts "the pair is #{pair}" }
