d067d68f30c63979036c90bcafe2691907e0680f
Elias Castañeda <e71az@icloud.com>
2020-06-22 17:00:25 -0600

    Created my_each_with_index Method

--- ruby.rb
+++ ruby.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
-# Some comment
 # Enumerable
 module Enumerable
   def my_each
@@ -14,7 +13,14 @@ module Enumerable
       end
     end
   end
+
+  def my_each_index
+    length.times do |i|
+      yield self[i], i
+    end
+  end
 end
+
 friends = %w[Jessica Michelle Elias Castañeda]
 friends.each { |friend| puts 'Hello, ' + friend }
 my_hash = { 'one' => 5, 'two' => 2 }
@@ -22,3 +28,11 @@ my_hash.my_each { |key, value| puts "#{key} is #{value}" }
 my_hash.my_each { |pair| puts "the pair is #{pair}" }
 my_hash.each { |key, value| puts "#{key} is #{value}" }
 my_hash.each { |pair| puts "the pair is #{pair}" }
+
+hash2 = {}
+xy = %w[cat dog wombat].my_each_index { |item, index| hash2[item] = index }
+p hash2
+
+hash3 = {}
+xyz = %w[cat dog wombat].each_with_index { |item, index| hash3[item] = index }
+p hash3
