7c0279ff4a33a4754e1c03fbdcb98b5190d2ba24
Elias Castañeda <e71az@icloud.com>
2020-06-23 09:01:04 -0600

    Added check if block received

--- ruby.rb
+++ ruby.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 
 # Enumerable
+
 module Enumerable
   def my_each
+    return to_enum(__method__) unless block_given?
+
     if is_a?(Array)
 
       length.times do |i|
@@ -12,16 +15,25 @@ module Enumerable
     else length.times do |i|
       yield [keys[i], values[i]]
     end
-
     end
   end
 
   def my_each_index
+    return to_enum(__method__) unless block_given?
     length.times do |i|
       yield self[i], i
     end
   end
 end
+
+# def my_each
+#   return to_enum(__method__) unless block_given?
+
+#   size.times { |i| yield(to_a[i]) }
+#   self
+# end
+
+# rubocop:disable Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
 
 friends = %w[Jessica Michelle Elias Castañeda]
 
@@ -31,11 +43,11 @@ my_hash = { 'one' => 5, 'two' => 2 }
 
 my_hash.my_each { |key, value| puts "#{key} is #{value}" }
 
-my_hash.my_each { |pair| puts "the pair is #{pair}" }
+p my_hash.my_each
 
 my_hash.each { |key, value| puts "#{key} is #{value}" }
 
-my_hash.each { |pair| puts "the pair is #{pair}" }
+# p my_hash.each
 
 hash2 = {}
 
