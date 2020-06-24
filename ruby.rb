c9407f21d021487309a523dac8b139ac47f6b652
Elias Castañeda <e71az@icloud.com>
2020-06-24 09:14:59 -0600

    Created test.ruby file, added rubocop exceptions

--- ruby.rb
+++ ruby.rb
@@ -1,5 +1,7 @@
 # Enumerable
 module Enumerable
+  # rubocop:disable Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
+
   def my_each
     return to_enum(__method__) unless block_given?
 
@@ -72,55 +74,12 @@ module Enumerable
 
   def my_count(*_args)
     @var = self
-    return @var.length - (self-_args).length
+    !_args.empty? ? @var.length - (self - _args).length : length
   end
+
+  # rubocop:enable Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
 end
 
-ary = [1,2,4,2,2,2,2,2,2]
-p ary.count               #=> 4
-p ary.my_count(2)            #=> 2
-=begin
-p %w[ant bear cat].my_none? { |word| word.length == 5 } #=> true
-p %w[ant bear cat].my_none? { |word| word.length >= 4 } #=> false
-p %w[ant bear cat].my_none?(/d/) #=> true
-p [1, 3.14, 42].my_none?(Float) #=> false
-p [].my_none? #=> true
-p [nil].my_none? #=> true
-p [nil, false].my_none? #=> true
-p [nil, false, true].my_none? #=> false
-=end
-
-# p %w[ant bear cat].my_any? { |word| word.length >= 3 } #=> true
-# p %w[ant bear cat].my_any? { |word| word.length >= 4 } #=> true
-# p %w[ant bear cat].my_any?(/d/) #=> false
-# p [nil, true, 99].my_any?(Integer) #=> true
-# p [nil, true, 99].my_any? #=> true
-# p [].my_any? #=> false
-
-# p %w[ant bear cat].my_all? { |word| word.length >= 3 } #=> true
-# p %w[ant bear cat].my_all? { |word| word.length >= 4 } #=> false
-# p [1, 2i, 3.14].my_all?(Numeric) #=> true
-# p [nil, true, 99].my_all? #=> false
-# p [].my_all? #=> true
-# p %w[asnt beart cat].my_all?(/t/) #=> false
-# friends = %w[Brian Brian Brian Leo Leila Brian Brian Arun Brian Sharon]
-# friends.my_select { |friend| friend == 'Brian' }
-# friends.my_select { |friend| friend != 'Brian' }
-
-# def my_each
-#   return to_enum(__method__) unless block_given?
-#   size.times { |i| yield(to_a[i]) }
-#   self
-# end
-# friends.each { |friend| puts 'Hello, ' + friend }
-# my_hash = { 'one' => 5, 'two' => 2 }
-# my_hash.my_each { |key, value| puts "#{key} is #{value}" }
-# p my_hash.my_each
-# my_hash.each { |key, value| puts "#{key} is #{value}" }
-# # p my_hash.each
-# hash2 = {}
-# xy = %w[cat dog wombat].my_each_with_index { |item, index| hash2[item] = index }
-# p hash2
-# hash3 = {}
-# xyz = %w[cat dog wombat].each_with_index { |item, index| hash3[item] = index }
-# p hash3
+ary = [1, 2, 4, 2, 2, 2, 2, 2, 2] 
+p ary.count #=> 4
+p ary.my_count(2) #=> 2
